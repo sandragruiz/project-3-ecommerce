4bebbb7e2dcb163a81898d002dbb5648d8739e38
unknown <sandra.gutierrez.ru@gmail.com>
2023-07-05 19:12:56 -0500

    pushing to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 //import components here.
+import React from 'react';
+import Homepage from './pages/Homepage/Homepage'
+
 import { Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
+      <Homepage />
       <Navbar/>
+      hello
     </div>
   );
 }
---
+++ client/src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+
+    return (
+    <div>
+        home
+    </div>
+);
+};
+
+export default Homepage;
