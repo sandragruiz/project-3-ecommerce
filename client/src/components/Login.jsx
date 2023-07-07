372f4bbba60b04076ee2765f2ada4c178fca4a59
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-07-06 20:34:57 -0500

    added login component code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -17,7 +17,31 @@ export const  Login = () => {
    
   };
 
-    return (
-    <div></div>
-);
-};
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
